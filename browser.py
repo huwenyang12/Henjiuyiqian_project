5f0c50296c0e44bcef3bac50bafebfa5c9659a7e
Wenyang Hu <hwy0821@yeah.net>
2025-11-19 16:21:29 +0800

    u

--- browser.py
+++ browser.py
@@ -3,7 +3,7 @@ import yaml
 import os
 import time
 
-from utils import try_click, safe_click
+from utils import safe_input, try_click, safe_click
 
 # 读取 config.yaml 配置
 CONFIG_FILE = os.path.join(os.path.dirname(__file__), "config.yaml")
@@ -31,11 +31,9 @@ class Browser:
                 print("已在控制台页面，无需登录")
                 return
             print("正在进行登录...")
-            cc.find_element(locator.login.login_username).set_text(cfg["login"]["username"])
-            time.sleep(1)
-            cc.find_element(locator.login.login_password).set_text(cfg["login"]["password"])
-            time.sleep(1)
-            cc.find_element(locator.login.button_登录).click()
+            safe_input(locator.login.login_username, cfg["login"]["username"])
+            safe_input(locator.login.login_password, cfg["login"]["password"])
+            safe_click(locator.login.button_登录)
             time.sleep(2)
             ele_logo_after = cc.wait_appear(locator.login.Logo, wait_timeout=5)
             print("登录成功!" if ele_logo_after else "登录失败!")
@@ -51,6 +49,7 @@ class Browser:
             safe_click(locator.query.span_财务会计)
             safe_click(locator.query.span_序时账)
             print("已进入序时账查询页面")
+            return
         except Exception as e:
             print(f"进入序时账查询页面失败：{e}")
             raise
